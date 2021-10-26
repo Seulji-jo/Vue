bc879cc920ba62e280e0a3260ad4449d6b79cacb
Seulji-jo <joseuling@gmail.com>
2021-10-26 13:35:40 +0900

    [add] Component with Slots 구현 방법과 활용처

--- vue-design-pattern/slots/src/App.vue
+++ vue-design-pattern/slots/src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <ul>
+      <!-- 1-1. slot(하나하나 정리) -->
       <!-- <Item>아이템 1</Item>
       <Item>아이템 2</Item>
       <Item>아이템 3</Item>
       <Item>아이템 4</Item>
       <Item>아이템 5</Item> -->
-      <Item v-for="item in items" :item='item' :key="item"></Item>
+      <!-- 1-2. slot(v-for를 이용한 정리) -->
+      <!-- <Item v-for="item in items" :key="item">{{ item }}</Item> -->
+      <!-- 1-3. 각각의 리스트 다른 형태 적용 -->
+      <Item>
+        아이템 1
+      </Item>
+      <Item>
+        아이템 2
+        <button>click me</button>
+      </Item>
+      <Item>
+        아이템 3
+        <img src="https://via.placeholder.com/200" alt="더미 이미지">
+      </Item>
+      <Item>
+        <div style='color:blue; font-size: 20px;'>아이템 4</div>
+      </Item>
+
+      <!-- 2. data props로 넘기는 방식 -->
+      <!-- <Item v-for="item in items" :item='item' :key="item"></Item>  -->
     </ul>
   </div>
 </template>
--- vue-design-pattern/slots/src/Item.vue
+++ vue-design-pattern/slots/src/Item.vue
@@ -1,14 +1,10 @@
 <template>
   <li>
-    <!-- <slot> -->
-      <!-- NOTE: 등록하는 곳에서 정의할 영역 -->
-    <!-- </slot> -->
-    {{ item }}
+    <slot>
+      <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
+    </slot>
+
+    <!-- props를 받아올때 -->
+    <!-- {{ item }} -->
   </li>
 </template>
-
-<script>
-export default {
-  props: ['item']
-}
-</script>
